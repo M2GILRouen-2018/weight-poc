ad5d44648a6da250de0b1591bdf86046790afdd3
Nicolas GILLE <nic.gille@gmail.com>
2018-02-12 15:35:47 +0100

    :pencil: Add postgresql feature.

--- src/test/java/io/univ/rouen/smartclass/weightpoc/simulators/DataTest.java
+++ src/test/java/io/univ/rouen/smartclass/weightpoc/simulators/DataTest.java
@@ -3,17 +3,43 @@ package io.univ.rouen.smartclass.weightpoc.simulators;
 import com.google.gson.Gson;
 import org.junit.Before;
 import org.junit.Test;
+import ru.yandex.qatools.embed.postgresql.EmbeddedPostgres;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import static ru.yandex.qatools.embed.postgresql.distribution.Version.Main.V9_6;
 
 public class DataTest {
 
     private Gson gson;
 
+    private Connection connection;
+
     @Before
-    public void setUp() {
+    public void setUp() throws IOException, SQLException {
         this.gson = new Gson();
+        // starting Postgres
+        final EmbeddedPostgres postgres = new EmbeddedPostgres(V9_6);
+        // predefined data directory
+        // final EmbeddedPostgres postgres = new EmbeddedPostgres(V9_6, "/path/to/predefined/data/directory");
+        final String url = postgres.start("localhost", 5432, "dbName", "userName", "password");
+
+        // connecting to a running Postgres and feeding up the database
+        connection = DriverManager.getConnection(url);
+        connection.createStatement().execute("CREATE TABLE data ( " +
+                "id integer NOT NULL" +
+                "stringData " +
+                "longData " +
+                "doubleData " +
+                "localDate " +
+                "listIntData " +
+                "PRIMARY KEY(id));");
+        connection.createStatement().execute("INSERT INTO data VALUES ('movie');");
     }
 
     @Test
