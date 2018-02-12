875123ff5cae71c1f243ddf6cf3dc077b4664961
Nicolas GILLE <nic.gille@gmail.com>
2018-02-12 10:31:12 +0100

    :tada: Initial commit.

---
+++ src/main/java/io/univ/rouen/smartclass/weightpoc/WeightPocApplication.java
@@ -0,0 +1,12 @@
+package io.univ.rouen.smartclass.weightpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeightPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeightPocApplication.class, args);
+	}
+}
